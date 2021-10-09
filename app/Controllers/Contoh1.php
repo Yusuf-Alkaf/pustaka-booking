316cf71f1bec169e473a521dcfd8476c30fcd09e
Yusuf-Alkaf <yusufalkaf11@gmail.com>
2021-10-09 12:37:41 +0700

    pustaka-booking & controller Contoh1.php

---
+++ app/Controllers/Contoh1.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Yusuf Alkaf<br>
+        Saya tinggal di daerah Bantul<br>
+        Olahraga yang saya sukai adalah bersepeda";
+    }
+}
