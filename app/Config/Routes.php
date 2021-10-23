c0eb4ce551e2616b70a3a3c4e8e7f54c34d711cc
Yusuf-Alkaf <yusufalkaf11@gmail.com>
2021-10-23 20:01:59 +0700

    Modify

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -33,6 +33,8 @@ $routes->setAutoRoute(true);
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
 $routes->get('/', 'Contoh1::index');
+$routes->get('/', 'Web::construct');
+$routes->get('/', 'Cinema::index');
 
 /*
  * --------------------------------------------------------------------
